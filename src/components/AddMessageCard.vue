99bb08adea4027c4b38a0007e5cd1605ea2e43c0
rajat arora <rajat.arora@hotmail.ca>
2017-10-31 19:18:28 +0000

    addded  partial giphy support

--- src/components/AddMessageCard.vue
+++ src/components/AddMessageCard.vue
@@ -4,22 +4,30 @@
                 <div class="avatar"></div>
                   <div class="text_wrapper">
                     <div class="text">
-                        {{event.message}}
+                      <div v-if="!checkIfGiphy(event.message)"> {{event.message}}</div>
+                        <img v-if="checkIfGiphy(event.message)" v-bind:src="event.message" ></img>
+
                     </div>
+                    <h4 class="pull-right"> From: {{event.email}}</h4>
                 </div>
             </li>
             
               <li class="message right appeared"  v-if="userEmail(event.email)">
                 <div class="avatar"></div>
                   <div class="text_wrapper">
+                   <button class="close" @click="removeJoke(index)"><span>&times;</span></button>
+
                     <div class="text">
-                       {{event.message}}
+                         <div v-if="!checkIfGiphy(event.message)"> {{event.message}}</div>
+                        <img v-if="checkIfGiphy(event.message)"  v-bind:src="event.message"></img>
                     </div>
                 </div>
             </li>
         </div>
 </template>
 <script>
+import {firebaseApp, eventsRef} from '../firebaseApp'
+
     export default{
         props:['event', 'index'],
         methods:{
@@ -29,7 +37,20 @@
             }else{
               return false
             }
-          }
+          },
+          removeJoke(index){
+                console.log("hello")
+                eventsRef.child(this.event.key).remove(function(error) {
+   // alert(error ? "Uh oh!" : "Success!");
+});
+            },
+            checkIfGiphy(text){
+              if(text.match(".gif") || text.match("giphy")){
+                return true
+              }else{
+                return false
+              }
+            }
         }
     }
 </script>
